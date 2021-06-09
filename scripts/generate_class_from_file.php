2dbd8ff988280dfb78186dd65d4760ede081eff0
pietraspawel <pawel.z.pietras@gmail.com>
2021-06-09 11:52:24 +0200

    up idiotoodporność
    w podawanej ścieżce można pominąć początkowy slash i końcowe .json

--- scripts/generate_class_from_file.php
+++ scripts/generate_class_from_file.php
@@ -13,6 +13,12 @@ require __DIR__ . "/../vendor/autoload.php";
 echo "you are here: " . __DIR__ . "\r\n";
 echo "enter path to .json file:\r\n";
 $path = trim(fgets(STDIN));
+if ($path[0] != "/") {
+    $path = "/{$path}";
+}
+if (substr($path, -5) != ".json") {
+    $path .= ".json";
+}
 $array = json_decode(file_get_contents(__DIR__ . $path), true);
 $class = Generator::generateClass($array);
 if (isset($array['class_name'])) {
